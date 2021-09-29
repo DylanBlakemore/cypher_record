20e5887cbadcaff04613573fb14cb5d5c31e54e6
Dylan Blakemore <dylan.blakemore@gmail.com>
2021-09-29 22:59:26 +0200

    Add error to load error message

--- lib/cypher_record.rb
+++ lib/cypher_record.rb
@@ -17,8 +17,8 @@ require_relative "cypher_record/entity_adapter"
 begin
   require "neo4j_ruby_driver"
   require_relative "cypher_record/engines/neo4j_engine"
-rescue LoadError
-  puts "Unable to load Neo4j Ruby driver. If this is a necessary dependency, make sure you include 'gem \"neo4j-ruby-driver\"' in your Gemfile"
+rescue LoadError => e
+  puts "Unable to load Neo4j Ruby driver with error #{e}. If this is a necessary dependency, make sure you include 'gem \"neo4j-ruby-driver\"' in your Gemfile"
 end
 
 # Models
